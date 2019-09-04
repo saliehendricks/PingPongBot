46d4848ca4f9b5036196f09314838f6e5e4e1592
Salie Hendricks <salie@safarinow.com>
2019-09-04 16:50:25 +0200

    Stubbed some ESP8266 TT Robot code

---
+++ esp/ttrobot/ttrobot.ino
@@ -0,0 +1,35 @@
+/**
+ * License: GPL
+ * Firmware Version:
+ * Date: September 2019
+ * 
+ * Control a Table Tennis robot over self hosted wifi connection.
+ * Features: ESP8266 / ESP32 firmware
+ * Drives 3 FPV Drone Motors
+ * Hosted Wifi page
+ *  
+ * */
+
+#include <Servo.h>
+
+//define the motor pins
+#define MTOP_PIN 3;
+#define MLEFT_PIN 4;
+#define MRIGHT_PIN 5;
+#define MFEEDER_PIN 6;
+
+//define LED pins
+#define LEDPINA 7;
+
+void setup() {
+
+    //setup wifi
+    //setup 3 launcher motors pwm
+    //setup 1 feeder motor
+    
+}
+
+void loop() {
+  
+
+}
