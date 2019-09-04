9d32499a3c71214c9727b45b0d072070e95e06d4
Salie Hendricks <salie@safarinow.com>
2019-09-04 16:51:20 +0200

    .

--- esp/ttrobot/ttrobot.ino
+++ esp/ttrobot/ttrobot.ino
@@ -1,3 +1,5 @@
+
+
 /**
  * License: GPL
  * Firmware Version:
@@ -10,26 +12,67 @@
  *  
  * */
 
-#include <Servo.h>
+#include <Servo.h>                //See: https://ubidots.com/blog/speed-control-for-brushless-motors-with-an-esp8266-2/
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
+#include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
+#include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
+#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
 //define the motor pins
-#define MTOP_PIN 3;
-#define MLEFT_PIN 4;
-#define MRIGHT_PIN 5;
-#define MFEEDER_PIN 6;
+int MTOP_PIN   = 3;
+int MLEFT_PIN  = 4;
+int MRIGHT_PIN = 5;
+int MFEED_PIN  = 6;
 
-//define LED pins
-#define LEDPINA 7;
+// //define LED pins
+ #define LEDPINA 12;
 
-void setup() {
+//define runtime objects
+WiFiManager wifiManager;
+Servo topSpin; 
+Servo leftSpin;
+Servo rightSpin;
+
+void setup() 
+{
+
+    Serial.begin(115000);
 
     //setup wifi
-    //setup 3 launcher motors pwm
+    Serial.println("Should save config");
+    wifiManager.autoConnect("PROTEA-TERMINATOR");
+        
+    //setup 3 launcher motors
+    topSpin.attach(MTOP_PIN,1000,2000);
+    leftSpin.attach(MTOP_PIN,1000,2000);
+    rightSpin.attach(MTOP_PIN,1000,2000);
+
     //setup 1 feeder motor
     
 }
 
-void loop() {
-  
+void loop() 
+{  
 
+}
+
+//Setup the system for topSpin at a specified speed
+//m|l|h (medium, low or high)
+void setTopSpin(char m = 'm')
+{
+    //set topspin speed
+    //reduce left/right speeds    
+}
+
+void setBackSpin(char m = 'm')
+{
+    //reduce topspin speed
+    //increase left/right speeds equal amounts
+}
+
+void setSideSpinChop(char m = 'm')
+{
+    //reduce topspin speed
+    //increase left speeds
+    //reduce right speed
 }
